33baab05ba8f0f129f04f123d1bf2ee2d11d6727
Muhammad Saleem <msaliim7@gmail.com>
2020-07-03 16:49:00 +0500

    added cart-icon and cart-dropdown components and also implemented cart reducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -7,7 +7,7 @@ import { connect } from 'react-redux';
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className='header'>
     <Link className='logo-container' to='/'>
       <Logo className='logo'></Logo>
@@ -31,12 +31,13 @@ const Header = ({ currentUser }) => (
       )}
       <CartIcon></CartIcon>
     </div>
-    <CartDropdown></CartDropdown>
+    {hidden ? null : <CartDropdown></CartDropdown>}
   </div>
 );
 
-const mapStateToProps = (state) => ({
-  currentUser: state.user.currentUser,
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden,
 });
 
 export default connect(mapStateToProps)(Header);
